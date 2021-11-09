71ad5ee2d7a785f0820dae42d3334fe0edb68a8e
TJ James <jamestw13@gmail.com>
2021-11-08 23:18:57 -0600

    tested MySQL/Node connection

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-// Dependencies
+// DEPENDENCIES
 // Express JS
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+// MySQL2
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+// Connect to SQL database
+const db = mysql.createConnection(
+	{
+		host: 'localhost',
+		// You MySQL username
+		user: 'root',
+		// Your MySQL password
+		password: 'Max8mumEffort',
+		database: 'election',
+	},
+	console.log('Connected to the election database.')
+);
 
 // Express routes
 
@@ -15,6 +30,10 @@ app.get('/', (req, res) => {
 	res.json({
 		message: 'Hello World',
 	});
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+	console.log(rows);
 });
 
 // Default response for any other request (Not Found)
