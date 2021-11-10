a7d56a9c095d755826ac06db55105a0979eac379
TJ James <jamestw13@gmail.com>
2021-11-09 23:25:56 -0600

    built voters table and seeds

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/api/candidates', (req, res) => {
 });
 
 // Get a single candidate
-app.get('/api/candidates/:id', (req, res) => {
+app.get('/api/candidate/:id', (req, res) => {
 	const sql = `SELECT candidates.*, parties.name 
   AS party_name 
   FROM candidates 
